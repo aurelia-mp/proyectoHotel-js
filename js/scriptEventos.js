fff0d6afb8ebded863c40244e580d722f938991d
aurelia-mp <aurelia.porolli@gmail.com>
2022-05-27 10:13:11 -0300

    subida del proyecto con dos opciones: cotizar y consultar

--- js/scriptEventos.js
+++ js/scriptEventos.js
@@ -1,9 +1,7 @@
-/* Versión reducida del cotizador, solo pude trabajar en la primera opción: Cotizar una estadía.
- Se dejan las opciones "Consultar" y "Cancelar reserva" para una próxima entrega */
-
  class Habitacion {
-    constructor(categoria, tarifaBaja, tarifaAlta, img){
+    constructor(categoria, descripcion, tarifaBaja, tarifaAlta, img){
         this.categoria = categoria;
+        this.descripcion = descripcion;
         this.tarifaBaja = tarifaBaja;
         this.tarifaAlta = tarifaAlta;
         this.img = img;
@@ -39,9 +37,9 @@ class Reserva {
 // INICIALIZACIÓN DE VARIABLES
 
 // Se cargan tres habitaciones de clase Habitacion en un array
-const standard = new Habitacion("Standard", 90, 100, "../assets/standard.jpg");
-const superior = new Habitacion("Superior", 110, 140, "../assets/superior.jpg");
-const suite = new Habitacion("Suite", 150, 300, "../assets/suite.jpg");
+const standard = new Habitacion("Standard", "Habitación doble matrimonial, con vistas a la ciudad", 90, 100, "../assets/standard.jpg");
+const superior = new Habitacion("Superior", "Habitación ubicada en los pisos superiores, con vistas al jardín", 110, 140, "../assets/superior.jpg");
+const suite = new Habitacion("Suite", "Suite de dos ambientes con terraza y jacuzzi", 150, 300, "../assets/suite.jpg");
 
 const habitaciones = [];
 
@@ -61,7 +59,7 @@ const reserva4 = new Reserva(4, "Leon Suarez", "03/03/2023", 2, "superior", 200)
 
 reservas.push(reserva1, reserva2, reserva3, reserva4);
 
-// Para probar la función filtrarReservas, se pasa la reserva 4 a inactiva
+// Se pasa la reserva 4 a inactiva
 reserva4.cancelarReserva();
     
 // Se inicializa numeroReserva en 5 para la próxima reserva del array
@@ -73,41 +71,60 @@ let cantidadNoches;
 let mes;
 let habitacionACotizar = {};
 let totalEstadia;
-let textoInicial = "Bienvenido/a a nuestro sistema de reservas online \n\nIngresa el número de opción deseada:\n\n 1. Cotizar una estadía\n 2. Consultar si tenés una reserva activa\n 3. Cancelar una reserva";
 let pasajero = "";
 let cotizacion;
+let filtro;
+
+// Definición de elementos del DOM
+
+// Menú Operaciones: Parte superior donde se muestran las dos opciones: Cotizador o Consultar Reserva
+let menuOperaciones = document.getElementById("menuOperaciones");
+
+// Opción 1 del menú : Cotizador
+let opcion1 = document.getElementById("opcion1");
+opcion1.addEventListener("click", () => deshabilitarSeccion(menuOperaciones));
+opcion1.addEventListener("click", iniciarCotizador);
+
+// Opción 2 del menú: Consultar reserva
+let opcion2 = document.getElementById("opcion2");
+opcion2.addEventListener("click", () => deshabilitarSeccion(menuOperaciones));
+opcion2.addEventListener("click", (e) => consultarReserva(e, reservas));
+
+// Boton Elegir otra Operación - Aparece al final de las secciones para volver a menú inicial
+let botonVolverInicio = document.getElementById("botonVolverInicio");
+
 
 // Funcion para insertar cards con las habitaciones
 function mostrarHabitaciones(lista){
     let contenedor = document.getElementById("habitaciones")
 
-    for (const habitacion of habitaciones){
+    for (const elemento of lista){
         let card = document.createElement("div")
         card.className= "row gx-5 whiteCard align-items-center flex-lg-column col-lg-6 col-xl-4 mb-3";
 
         card.innerHTML = `<div class="col-md-5 col-lg-12 d-flex justify-content-center">
-                            <img src="${habitacion.img}" class="img-fluid rounded-start" alt="Foto de una habitacion ${habitacion.categoria}">
+                            <img src="${elemento.img}" class="img-fluid rounded-start" alt="Foto de una habitacion ${elemento.categoria}">
                             </div>
                             <div class="col-md-7 col-lg-12">
                             <div class="card-body">
-                                <h3 class="card-title titulo--xl text-uppercase">${habitacion.categoria}</h3>
+                                <h3 class="card-title titulo--xl text-uppercase">${elemento.categoria}</h3>
                                 <div class="card-text fw-light">
-                                    <p>${habitacion.categoria}</p>
+                                    <p>${elemento.descripcion}</p>
                                 </div>
                                 <div class="card-footer datosReco">
                                     <table>
                                         <tr>
                                             <td>Tarifa Temporada Alta</td>
-                                            <td>${habitacion.tarifaAlta}</td>
+                                            <td>${elemento.tarifaAlta}</td>
                                         </tr>
                                         <tr>
                                             <td>Tarifa Temporada Alta</td>
-                                            <td>${habitacion.tarifaBaja}</td>
+                                            <td>${elemento.tarifaBaja}</td>
                                         </tr>
                                     </table>
                                 </div>
                             </div>
-                            </div>
+                        </div>
         `
         contenedor.append(card);
     }
@@ -127,9 +144,28 @@ function insertarCategorias(lista){
     }
 }
 
+//////////////////////////
+// OPCION 1: COTIZADOR //
+/////////////////////////
+function iniciarCotizador(){
+    let cotizador = document.getElementById("cotizador");
+    cotizador.classList.toggle("oculto");
+
+    // Al apretar el botón "Elegir otra operación", se oculta la sección cotizador y se vuelve a habilitar el menú inicial
+    botonVolverInicio.classList.toggle("oculto");
+
+    // Al enviar el formulario, se cotiza la estadía con los datos ingresados
+    let formulario = document.getElementById("formulario");
+    formulario.addEventListener("submit", cotizarEstadia);
+}
+
+
 // Funcion cotizar estadía - Asociada al evento SUBMIT en el formulario de cotización
 function cotizarEstadia(e){
     e.preventDefault();
+
+    let botonCotizar = document.getElementById("botonCotizar");
+    botonCotizar.disabled=true;
 
     let pasajero = document.querySelector("#nombre").value;
     let numeroCategoria = document.querySelector("#menuCategoria").value;
@@ -144,7 +180,6 @@ function cotizarEstadia(e){
 
     categoria = habitaciones[numeroCategoria];
 
-    //AGREGAR UNA VALIDACION PARA QUE EL USUARIO NO PUEDA INGRESAR UNA CANTIDAD DE NOCHES NULA O NEGATIVA
     cotizacion = document.createElement("p");
     formulario.append(cotizacion);
 
@@ -161,22 +196,21 @@ function cotizarEstadia(e){
     // Grabar la cotización en un objeto provisorio "Reserva"
     let reservaNueva = new Reserva (numeroReserva, pasajero, fechaLlegada, cantidadNoches, categoria["categoria"], totalEstadia);
 
-    // Mostrar los botones confirmar y reset
+    // Mostrar los botones confirmar // reset // Volver al inicio
     let botones = document.getElementById("botones");
     let botonConfirmar = document.getElementById("botonConfirmar");
     let botonReset = document.getElementById("botonReset");
 
     botones.classList.toggle("oculto"); 
 
+    // Al apretar el boton confirmar, se carga la reserva nueva
     botonConfirmar.onclick = () => {confirmarReserva(reservaNueva, pasajero)}
 
-    botonReset.onclick = () => {borrarTodo()};
-}
-
-function borrarTodo(){
-    cotizacion.remove();
-    botones.classList.toggle("oculto");
-    formulario.reset();
+    // Al apretar el boton Reset, se borra los datos del formulario y se re-habilita el botón "submit"
+    botonReset.onclick = () => {
+        reiniciarCotizador()
+        botonCotizar.disabled = false;
+    };
 }
 
  // Function Confirmar reserva
@@ -205,8 +239,8 @@ function displayReservaHTML(reserva){
     let seccionReservas = document.createElement("section");
     seccionReservas.innerHTML="<h2>Tu nueva reserva:</h2>"
     seccionReservas.id="reserva";
-    let principal = document.getElementById("principalReservar")
-    principal.append(seccionReservas);
+    let cotizador = document.getElementById("cotizador")
+    cotizador.append(seccionReservas);
     
     let tablaReservaNueva = document.createElement("table");
     let tablaReservaNuevaHead =document.createElement("thead");
@@ -241,7 +275,102 @@ function displayReservaHTML(reserva){
 }
 
 
-// Function para hacer aparecer o desaparecer un elemento
+function volverAlInicio(seccionActual){
+    if (seccionActual == "cotizador"){
+        cotizacion.remove();
+        botones.classList.toggle("oculto");
+        formulario.reset();
+    }
+    else if(seccionActual == "consultas"){
+        respuesta.remove();
+        formNumero.reset();
+        formNombre.reset();
+        consultas.classList.toggle("oculto");
+    }
+}
+
+function reiniciarCotizador(){
+    if(cotizacion){
+        cotizacion.remove();
+    }
+    botones.classList.toggle("oculto");
+    formulario.reset();
+    botonCotizar.disabled=false;
+}
+
+/////////////////////////
+// OPCION 2 : CONSULTAS//
+/////////////////////////
+function consultarReserva(e, reservas){
+    e.preventDefault();
+    botonVolverInicio.classList.toggle("oculto");
+    let consultas = document.getElementById("consultas");
+    consultas.classList.toggle("oculto");
+
+    // Mostrar campo nombre o campo número de reserva según la opcion elegida en el checkbox
+    let opcionNombre = document.getElementById("opcionNombre");
+    let opcionId = document.getElementById("opcionId");
+    opcionNombre.onclick = () => {filtrarReservas(reservas, 1)};
+    opcionId.onclick = () => {filtrarReservas(reservas, 2)};
+};
+
+function filtrarReservas(array, filtro){
+    opcionesConsulta.classList.toggle("oculto");
+
+    // Opción 1 : Buscar por nombre
+    if (filtro == 1){
+        let formNombre = document.getElementById("formNombre");
+        formNombre.classList.toggle("oculto"); 
+        formNombre.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let nombreUsuario = document.querySelector("#nombreUsuario").value;
+            let reservasFiltradas = array.filter((reserva) => reserva.pasajero == nombreUsuario);
+            mostrarReservas(reservasFiltradas, "nombre", formNombre);
+        })
+    }       
+    
+    else if (filtro == 2){
+        let formNumero = document.getElementById("formNumero");
+        formNumero.classList.toggle("oculto"); 
+        formNumero.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let id = document.querySelector("#idReserva").value;
+            let reservasFiltradas = array.filter((reserva) => reserva.numero == id);
+            mostrarReservas(reservasFiltradas, "numero", formNumero);
+        })
+    }
+}
+
+function mostrarReservas(array, parametro, seccion){
+    let respuesta = document.getElementById("respuestaConsulta")
+
+    // Caso 1: El array de reservas filtradas está vacío
+    if(array.length == 0){
+        // Si no es la primera consulta, en vez de crearse un nuevo elemento, se pisa el element existente
+           respuesta.innerHTML= `No hay reservas activas con este ${parametro}`;   
+    }
+    // Caso 2: Hay reservas con el filtro
+    else{
+        // Se verifica si las reservas encontradas están activas
+        let reservasFiltradasActivas = array.filter(reserva => reserva.activa);
+        if (reservasFiltradasActivas.length == 0){
+            respuesta.innerHTML= `La(s) reserva(s) que encontramos con este ${parametro } no está(n) activa(s)`;
+            // return false;
+        }
+        else{
+            respuesta.innerHTML= `Hay reserva(s) con este ${parametro} : \n`;
+            reservasFiltradasActivas.forEach(reserva => {
+                let impresionReserva = document.createElement("p")
+                impresionReserva.innerHTML = `Reserva # ${reserva.numero}\nFecha de llegada ${reserva.checkin}\nCantidad de noches: ${reserva.noches}\nCategoria:  ${reserva.categoria}\nValor total de la estadía:  ${reserva.totalEstadia}`;
+                respuesta.append(impresionReserva);
+            })
+            // return true;
+        }
+        }
+}
+
+
+// Function para hacer aparecer o desaparecer un elemento 
 function toggleDisplay(elemento){
     if (elemento.style.display == "block" || elemento.style.display == ""){
         elemento.style.display = "none";
@@ -251,6 +380,22 @@ function toggleDisplay(elemento){
     }
 }
 
+// Function para pasar una sección a "DESHABILITADA" - Cambia la opacidad y deshabilita los botones
+function deshabilitarSeccion(seccion){
+    seccion.style.opacity = 0.5;
+    let bt = seccion.getElementsByTagName("button");
+    for (const boton of bt){
+        boton.disabled = true;
+    }
+}
+
+function habilitarSeccion(seccion){
+    seccion.style.opacity = 1;
+    let bt = seccion.getElementsByTagName("button");
+    for (const boton of bt){
+        boton.disabled = false;
+    }
+}
 
 // Agrega una lista de habitaciones en forma de cards
 mostrarHabitaciones(habitaciones)
@@ -258,7 +403,14 @@ mostrarHabitaciones(habitaciones)
 // Genera dinámicamente el dropdown para elegir la categoría de habitación
 insertarCategorias(habitaciones)
 
-// Al enviar el formulario, se cotiza la estadía con los datos ingresados
-let divCotizacion = document.getElementById("divCotizacion");
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", cotizarEstadia);
+
+
+
+
+
+
+
+
+
+
+
